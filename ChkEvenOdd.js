40b124d54573b5ad6a5a43c2ae628e3077dc869c
Aditya Shejwal <adityashejwal016@gmail.com>
2026-03-29 13:25:20 +0530

    Add files via upload

---
+++ ChkEvenOdd.js
@@ -0,0 +1,17 @@
+function CheckEvenOdd(no)
+{
+    if(no % 2 == 0)
+    {
+        console.log("Its Even Number");
+    }
+    else
+    {
+        console.log("its Odd Number");
+    }
+
+}
+function main()
+{
+    CheckEvenOdd(22);
+}
+main();
