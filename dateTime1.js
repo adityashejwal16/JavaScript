20a4866bbe18bb27a2c1700f2fc00ab789b66342
Aditya Shejwal <adityashejwal016@gmail.com>
2026-03-29 13:25:40 +0530

    Add files via upload

---
+++ dateTime1.js
@@ -0,0 +1,9 @@
+
+let a = new Date();
+
+console.log(a.getDate());   // 27
+console.log(a.getMonth());   // 2
+console.log(a.getFullYear());  // 2026
+console.log(a.getHours());     // 14
+console.log(a.getMinutes());   // 32
+
