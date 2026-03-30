153a10995a1ba84d3df91c7bc02cad06767f3e84
Aditya Shejwal <adityashejwal016@gmail.com>
2026-03-30 16:27:12 +0530

    Add files via upload

---
+++ practice2.js
@@ -0,0 +1,12 @@
+
+var age = true;
+
+console.log(age);
+console.log(typeof(age));    // boolean
+
+
+let str = "Aadi";
+
+console.log(str);
+console.log(typeof(str));
+console.log(str.length);   // 4
