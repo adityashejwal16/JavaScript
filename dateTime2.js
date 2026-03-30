3b8206d43f9d11ac15dbe686abb922c4f914f83c
Aditya Shejwal <adityashejwal016@gmail.com>
2026-03-30 16:25:00 +0530

    Add files via upload

---
+++ dateTime2.js
@@ -0,0 +1,15 @@
+// Date And Time
+
+let a = new Date();
+
+console.log( "Date is : "+ a.getDate());   // 27
+console.log("Month is : "+ a.getMonth());   // 2
+console.log("Year Is :" + a.getFullYear());  // 2026
+
+// Month Output is 2 because
+
+// jan   0
+// feb   1
+// mar   2
+
+// Moths are 0 based indexed
